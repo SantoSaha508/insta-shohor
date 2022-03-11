b5c606bf3216492d2664642530b812c1fd35818f
shanto <santo15-10959@diu.edu.bd>
2022-03-11 17:59:12 +0600

    fixed report section

--- js/app.js
+++ js/app.js
@@ -46,7 +46,7 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "none";
 
         displayLikedPosts();
-    } else {
+    } else{
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
@@ -158,10 +158,13 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    const repot = document.getElementById( "reported" );
+    repot.innerHTML = "";
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
-        document.getElementById( "reported" ).appendChild(div);
+        repot.appendChild(div);  
     });
+    
 };
 
 const loadPosts = async () =>{
